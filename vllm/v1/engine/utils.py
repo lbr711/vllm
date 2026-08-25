6c2404d4f35d20a75cf4160ebbec3398f3e9589b
lbr711 <liuboru1@huawei.com>
2026-08-25 22:00:02 +0800

    snapshot support

--- vllm/v1/engine/utils.py
+++ vllm/v1/engine/utils.py
@@ -1245,6 +1245,15 @@ def wait_for_engine_startup(
         and not parallel_config.data_parallel_external_lb
     )
 
+    parallel_config_keys = (
+        "data_parallel_master_ip",
+        "data_parallel_master_port",
+        "_data_parallel_master_port_list",
+        "data_parallel_size",
+    )
+    if parallel_config._snapshot_data_parallel_port_list is not None:
+        parallel_config_keys += ("_snapshot_data_parallel_port_list",)
+
     if proc_manager is not None:
         for sentinel in proc_manager.sentinels():
             poller.register(sentinel, zmq.POLLIN)
@@ -1315,12 +1324,7 @@ def wait_for_engine_startup(
                     addresses=addresses,
                     parallel_config={
                         k: getattr(parallel_config, k)
-                        for k in (
-                            "data_parallel_master_ip",
-                            "data_parallel_master_port",
-                            "_data_parallel_master_port_list",
-                            "data_parallel_size",
-                        )
+                        for k in parallel_config_keys
                     }
                     if coordinated_dp
                     else {},
